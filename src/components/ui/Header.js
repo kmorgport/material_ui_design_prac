2f5a731b21b49c7ac36fb48d271a414e240520d0
kmorgport <kmorgan0057@gmail.com>
2022-01-21 15:59:54 -0600

    started working on AppBar

---
+++ src/components/ui/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+
+const Header = (props) => {
+
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                Arc Development
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header
