3a940386679ad582e843fbb7359c20371bdf6e8d
kmorgport <kmorgan0057@gmail.com>
2022-01-24 15:29:47 -0600

    tabs and styles

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography  from "@material-ui/core/Typography";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import {Tabs, Tab } from '@material-ui/core'
+
+import logo from '../../assets/logo.svg'
+import { makeStyles } from "@material-ui/core";
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -18,19 +21,44 @@ function ElevationScroll(props) {
     });
   }
 
+  const useStyles = makeStyles(theme => ({
+      toolbarMargin: {
+          ...theme.mixins.toolbar,
+          marginBottom: "3em"
+      },
+      logo:{
+          height: "7em"
+      },
+      tabContainer:{
+          marginLeft: 'auto'
+      },
+      tab:{
+          fontFamily: "Raleway"
+      }
+  }))
+
 
 const Header = (props) => {
+    const classes = useStyles()
 
     return (
+        <React.Fragment>
         <ElevationScroll>
             <AppBar position="fixed">
-                <Toolbar>
-                    <Typography variant="h3" color="secondary">
-                    Arc Development
-                    </Typography>
+                <Toolbar disableGutters>
+                    <img src={logo} className={classes.logo} alt="company logo"/>
+                    <Tabs className={classes.tabContainer}>
+                        <Tab className={classes.tab} label="Home"/>
+                        <Tab className={classes.tab} label="Services"/>
+                        <Tab className={classes.tab} label="The Revolution"/>
+                        <Tab className={classes.tab} label="About Us"/>
+                        <Tab className={classes.tab} label="Contact Us"/>
+                    </Tabs>
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
+        <div className={classes.toolbarMargin}/>
+        </React.Fragment>
     )
 }
 
