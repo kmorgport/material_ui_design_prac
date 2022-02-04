3e5d62b6b4184a1ea3b7a67365c4b25bc90a354b
kmorgport <kmorgan0057@gmail.com>
2022-02-04 16:52:37 -0600

    implementing drawer function

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect } from "react";
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import {Tabs, Tab, Button, Menu, MenuItem } from '@material-ui/core'
+import {Tabs, Tab, Button, Menu, MenuItem, SwipeableDrawer, IconButton } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
 import { makeStyles, useMediaQuery, useTheme } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 
 function ElevationScroll(props) {
@@ -74,37 +75,46 @@ function ElevationScroll(props) {
           "&:hover": {
               opacity: 1
           }
+      },
+      drawerIconContainer: {
+          "&:hover": {
+              backgroundColor: "transparent"
+          }
       }
   }))
 
 
 const Header = (props) => {
     const theme = useTheme();
+    // const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+    const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent); 
     const matches = useMediaQuery(theme.breakpoints.down("md"))
     const classes = useStyles();
     const [value, setValue] = useState(0)
+
+    const [openDrawer, setOpenDrawer] = useState(false);
     const [ anchorEl, setAnchorEl ] = useState(null);
-    const [ open, setOpen ] = useState(false)
+    const [ openMenu, setOpenMenu ] = useState(false)
     const [ selectedIndex, setSelectedIndex ] = useState()
 
-    const handleChange = (e, value ) => {
-        setValue(value)
+    const handleChange = (e, newValue ) => {
+        setValue(newValue)
     }
 
     const handleClick = (e) => {
         setAnchorEl(e.currentTrigger)
-        setOpen(true)
+        setOpenMenu(true)
     }
 
     const handleMenuItemClick = (e, i ) => {
         setAnchorEl(null)
-        setOpen(false)
+        setOpenMenu(false)
         setSelectedIndex(i)
     }
 
     const handleClose = (e) => {
         setAnchorEl(null)
-        setOpen(false)
+        setOpenMenu(false)
     }
 
     const menuOptions = [
@@ -217,7 +227,7 @@ const Header = (props) => {
                     <Menu
                         id="simple-menu"
                         anchorEl={anchorEl}
-                        open={open}
+                        open={openMenu}
                         onClose={handleClose}
                         classes={{ paper: classes.menu }}
                         MenuListProps={{onMouseLeave: handleClose}}
@@ -239,7 +249,26 @@ const Header = (props) => {
                      </Menu>
         </React.Fragment>
     )
-
+    
+    const drawer = (
+        <React.Fragment>
+            <SwipeableDrawer disableBackdropTransition={!iOS}
+            disableDiscovery={iOS}
+            open={openDrawer}
+            onClose={()=> setOpenDrawer(false)}
+            onOpen= {()=>setOpenDrawer(true)}
+            >
+                Example Drawer
+            </SwipeableDrawer>
+            <IconButton 
+                className={classes.drawerIconContainer}
+                onClick={()=> setOpenDrawer(!openDrawer)}
+                disableRipple
+            >
+                <MenuIcon/>
+            </IconButton>
+        </React.Fragment>
+    )
     return (
         <React.Fragment>
             <ElevationScroll>
@@ -248,7 +277,7 @@ const Header = (props) => {
                         <Button className={classes.logoContainer} onClick={()=>setValue(0)} component={Link} to="/">
                         <img src={logo} className={classes.logo} alt="company logo"/>
                         </Button>
-                        { matches ? null : tabs }
+                        { matches ? drawer : tabs }
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
