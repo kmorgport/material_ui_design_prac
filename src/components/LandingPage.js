e48cdea7ae04f211f98bb8ebf9fe105c9c5c5e16
kmorgport <kmorgan0057@gmail.com>
2022-02-17 16:56:43 -0600

    landing page styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,11 +7,42 @@ import ButtonArrow from '../components/ui/ButtonArrow'
 import animationData from '../animations/landinganimation/data'
 
 const useStyles = makeStyles( theme => ({
-
+        animation: {
+            madWidth: "50em",
+            minWidth: "21em",
+            marginTop: "2em",
+            marginLeft: "10%"
+        },
+        estimateButton: {
+            ...theme.typography.estimate,
+            backgroundColor: theme.palette.common.orange,
+            borderRadius: 50,
+            height: 45,
+            width: 145,
+            marginRight: 40,
+            "&:hover": {
+                backgroundColor: theme.palette.secondary.light
+            }
+        },
+        buttonContainer: {
+            marginTop: "1em",
+        },
+        learnButtonHero: {
+            borderColor: theme.palette.common.blue,
+            color: theme.palette.common.blue,
+            borderWidth: 2,
+            textTransform: "none",
+            borderRadius: 50,
+            fontFamily: "Roboto",
+            fontWeight: "bold",
+            fontSize: "0.9rem",
+            height: 45,
+            width: 145
+        }
 }))
 
 const LandingPage  = () => {
-    // const classes = useStyles();
+    const classes = useStyles();
 
     const defaultOptions = {
         loop: true,
@@ -27,13 +58,13 @@ const LandingPage  = () => {
             <Grid item>
                 <Grid container justify="flex-end" alignItems="center" direction="row">
                     <Grid sm item >
-                        <Typography variant="h2" align="center"></Typography>
-                        <Grid container>
+                        <Typography variant="h2" align="center">Bringing West Coast Technology to the Midwest</Typography>
+                        <Grid container justify="center" className={classes.buttonContainer}>
                             <Grid item>
-                                <Button variant="container">Free Estimate</Button>
+                                <Button className={classes.estimateButton} variant="container">Free Estimate</Button>
                             </Grid>
                             <Grid item>
-                                <Button variant="outlined">
+                                <Button variant="outlined" className={classes.learnButtonHero}>
                                     Learn More
                                     <ButtonArrow width={15} height={15} fill="red"/>
 
@@ -41,7 +72,7 @@ const LandingPage  = () => {
                             </Grid>
                         </Grid>
                     </Grid>
-                    <Grid sm item>
+                    <Grid className={classes.animation} sm item>
                         <Lottie 
                             options={defaultOptions} 
                             height={"100%"} 
