12c16ee3b88e94d2320d0580a9ede1621f6f3f00
kmorgport <kmorgan0057@gmail.com>
2022-02-27 15:28:30 -0600

    starting Services page

---
+++ src/components/Services.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography, Button } from '@material-ui/core'
+
+const useStyles = makeStyles( theme => ({
+
+}))
+
+export default Services = () => {
+    const classes = useStyles();
+
+    return (
+        <Grid container direction="column">
+
+        </Grid>
+    )
+}
