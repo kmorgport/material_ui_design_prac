fd8c33abef0f935c727498fc927c156960439246
kmorgport <kmorgan0057@gmail.com>
2022-03-17 20:05:42 -0500

    editing revolution page

--- src/components/Revolution.js
+++ src/components/Revolution.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Lottie from 'react-lottie';
 import { Link } from 'react-router-dom';
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import { Grid, IconButton, Typography, useMediaQuery, Hidden} from '@material-ui/core'
 
 import vision from '../assets/vision.svg'
+import technologyAnimation from '../animations/technologyAnimation/data.json'
 
 const useStyles = makeStyles(theme => ({
     rowContainer:{
@@ -19,6 +21,15 @@ const useStyles = makeStyles(theme => ({
 const Revolution = ({}) => {
     const classes = useStyles();
 
+    const defaultOptions = {
+        loop: false,
+        autoplay: false,
+        animationData: technologyAnimation,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+
     return (
         <Grid container direction="column">
             <Grid item className={classes.rowContainer}>
@@ -26,11 +37,11 @@ const Revolution = ({}) => {
                 The Revolution
             </Typography>
             </Grid>
-            <Grid item container className={classes.rowContainer}>
+            <Grid item container className={classes.rowContainer} alignItems="center">
                 <Grid item>
-                    <img src={vision} alt="mountain through binoculars"/>
+                    <img src={vision} alt="mountain through binoculars" style={{ maxWidth: "40em", marginRight:"5em"}} />
                 </Grid>
-                <Grid item container direction="column" alignItems="center" lg>
+                <Grid item container direction="column" style={{ maxWidth:"40em"}} lg>
                     <Grid item lg>
                         <Typography align="right" variant="h4" gutterBottom>
                             Vision
@@ -67,6 +78,69 @@ const Revolution = ({}) => {
                             towards that goal.
                         </Typography>
                     </Grid>
+                </Grid>
+            </Grid>
+            <Grid item container className={classes.rowContainer} alignItems="center">
+                <Grid item container direction="column" style={{ maxWidth:"40em"}} lg>
+                    <Grid item lg>
+                        <Typography variant="h4" gutterBottom>
+                            Technology
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="body1" paragraph>
+                            In 2013, Facebook invented a new way of building websites. This
+                            new system, React.js, completely revolutionizes the process and
+                            practice of website development.
+                        </Typography>
+                        <Typography variant="body1" paragraph>
+                            Instead of chaining together long individual pages, like
+                            traditional websites, React websites are built with little chunks
+                            of code called components. These components are faster, easier to
+                            maintain, and are easily reused and customized, each serving a
+                            singular purpose.
+                        </Typography>
+                        <Typography variant="body1" paragraph>
+                            Two years later they shocked the world by releasing a similar
+                            system, React Native, for producing iOS and Android apps. Instead
+                            of having to master two completely separate development platforms,
+                            you can leverage the knowledge you already possessed from building
+                            websites and reapply it directly! This was a huge leap forward.
+                        </Typography>
+                        <Typography variant="body1" paragraph>
+                            This technology is now being used by companies like AirBnB,
+                            Microsoft, Netflix, Pinterest, Skype, Tesla, UberEats, and when
+                            Facebook purchased Instagram large portions of it were even
+                            rebuilt using React.
+                        </Typography>
+                        <Typography variant="body1" paragraph>
+                            Developers have since built on top of these systems by automating
+                            project setup and deployment, allowing creators to focus as much
+                            as possible on their work itself.
+                        </Typography>
+                        <Typography variant="body1" paragraph>
+                            These technical advancements translate into savings by
+                            significantly reducing the workload and streamlining the workflow
+                            for developing new pieces of software, while also lowering the
+                            barrier to entry for mobile app development.
+                        </Typography>
+                        <Typography variant="body1" paragraph>
+                            This puts personalization in your pocket — faster, better, and
+                            more affordable than ever before.
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid item lg container justify="flex-end">
+                    <Lottie 
+                        options={defaultOptions} 
+                        isStopped={true} 
+                        style={{maxWidth: "40em", margin: 0}} 
+                    />
+                </Grid>
+            </Grid>
+            <Grid item container direction="row" className={classes.rowContainer}>
+                <Grid item>
+                    
                 </Grid>
             </Grid>
         </Grid>
