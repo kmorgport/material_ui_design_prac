40ea1aaa9dc685ba58b78c6dd5317445f0e8350a
kmorgport <kmorgan0057@gmail.com>
2022-04-01 14:34:31 -0500

    adding cloud functions

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer")
+
+const config = functions.config()
+const cors = require("cors")({ origin: true })
+admin.initializeApp()
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: config.user.email,
+        pass: config.user.password
+    }
+});
+
+let mailOptions = {
+    from: 'Arc Development',
+    to: "otheremail",
+    subject: "testing Node Mailer",
+    text: "Test successful"
+}
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.sendMail = functions.https.onRequest((request, response) => {
+    cors(request, respoonse, ()=> {
+        transporter.sendMail(mailOptions, error => {
+            if(error){
+                response.send(error)
+            }else{
+                response.send("Message sent successfully")
+            }
+        })
+    })
+});
